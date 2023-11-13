994798434b8769ea86bc4f347d612311ee5a4b50
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-11-13 17:31:02 +0200

    add jks

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -10,14 +10,11 @@ set -x
 
 cd ..
 git clone -b stable https://github.com/flutter/flutter.git
-# shellcheck disable=SC2006
-# shellcheck disable=SC2155
 export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
 flutter doctor
 
-# shellcheck disable=SC2006
 echo "Installed flutter to `pwd`/flutter"
 
 flutter pub get
