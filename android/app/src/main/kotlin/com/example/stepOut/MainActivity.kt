80b5d572b585fdcce0bf303435d46fdecb325588
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-11-13 12:57:09 +0200

    add AppCenter

---
+++ android/app/src/main/kotlin/com/example/stepOut/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.eBrandstepOut.stepOut
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCenter.start(
+            getApplication(), "f66a71a7-68e7-48d1-b792-9f953423dbec",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
