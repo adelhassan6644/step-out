6bfbfd9ea030a69673e9207c04d486d08db58161
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-11-13 15:43:02 +0200

    add AppCenter

--- android/app/src/main/kotlin/com/example/stepOut/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stepOut/MainActivity.kt
@@ -8,11 +8,11 @@ import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        AppCenter.start(
-            getApplication(), "f66a71a7-68e7-48d1-b792-9f953423dbec",
-            Analytics::class.java, Crashes::class.java
-        )
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        AppCenter.start(
+//            getApplication(), "f66a71a7-68e7-48d1-b792-9f953423dbec",
+//            Analytics::class.java, Crashes::class.java
+//        )
+//    }
 }
