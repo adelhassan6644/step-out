5a80b1aa8e00bd41508286e321e379bc43a88818
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-11-13 17:45:28 +0200

    add jks

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\New Flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\adelh\Documents\GitHub\step-out"
+export "FLUTTER_APPLICATION_PATH=C:\Users\adelh\Documents\GitHub\stepOut"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
