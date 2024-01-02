75528590c22c2cc7494475c526b9a1c7745f900f
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2024-01-02 22:11:46 +0200

    update language bottom sheet

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,10 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +14,10 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyA2xA7d84QkR0_0OPjdDRLfu--YbjJkSa8")
     GeneratedPluginRegistrant.register(with: self)
+    AppCenter.start(withAppSecret: "6e0c0431-d34d-4217-9a6b-64836aba6f84", services:[
+      Analytics.self,
+      Crashes.self
+    ])
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
